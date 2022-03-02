cddfd99d2383f43409e06186344ea098a9b4b68e
BrandedNomad <info@brandednomad.com>
2022-03-02 14:07:34 +0700

    feat: added menu on shoelist

--- app/src/main/java/com/example/shoestore/MainActivity.kt
+++ app/src/main/java/com/example/shoestore/MainActivity.kt
@@ -2,12 +2,47 @@ package com.example.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
+import com.example.shoestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        //Find nav controller
+        val navController = this.findNavController(R.id.navHostFragment)
+
+
+        //connect to action bar
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
+        //Configure
+        appBarConfiguration = AppBarConfiguration(navController.graph)
 
     }
+
+    //Controlls behaviour of up navigation
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return NavigationUI.navigateUp(navController,appBarConfiguration)
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/shoestore/screens/shoelist/ShoeListFragment.kt
+++ app/src/main/java/com/example/shoestore/screens/shoelist/ShoeListFragment.kt
@@ -1,12 +1,12 @@
 package com.example.shoestore.screens.shoelist
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.shoestore.R
 import com.example.shoestore.databinding.ShoelistFragmentBinding
 
@@ -33,6 +33,20 @@ class ShoeListFragment: Fragment() {
             findNavController().navigate(ShoeListFragmentDirections.actionShoeListDestinationToDetailsDestination())
         }
 
+        setHasOptionsMenu(true)
+
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.logout_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
+
 }
